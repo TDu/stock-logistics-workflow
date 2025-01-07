1423ae38d5b7b5e15a190a72f719addf278c45e4
Thierry Ducrest <thierry@ducrest.net>
2025-01-07 13:12:21 +0100

    fixup! Add stock_split_picking_kit

--- stock_split_picking_kit/tests/test_stock_split_picking_kit.py
+++ stock_split_picking_kit/tests/test_stock_split_picking_kit.py
@@ -13,37 +13,12 @@ class TestStockSplitPickingKit(SavepointCase):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
 
+        cls.warehouse = cls.env.ref("stock.warehouse0")
         cls.src_location = cls.env.ref("stock.stock_location_stock")
         cls.dest_location = cls.env.ref("stock.stock_location_customers")
         cls.product = cls.env["product.product"].create({"name": "Test product"})
         cls.partner = cls.env["res.partner"].create({"name": "Test partner"})
-        cls.picking = cls.env["stock.picking"].create(
-            {
-                "partner_id": cls.partner.id,
-                "picking_type_id": cls.env.ref("stock.picking_type_out").id,
-                "location_id": cls.src_location.id,
-                "location_dest_id": cls.dest_location.id,
-            }
-        )
-        cls.move = cls.env["stock.move"].create(
-            {
-                "name": "/",
-                "picking_id": cls.picking.id,
-                "product_id": cls.product.id,
-                "product_uom_qty": 10,
-                "product_uom": cls.product.uom_id.id,
-                "location_id": cls.src_location.id,
-                "location_dest_id": cls.dest_location.id,
-            }
-        )
-
-        # cls.seller = cls.env.ref("base.res_partner_1")
-        cls.customer = cls.env.ref("base.res_partner_2")
-        # cls.sale_order_model = cls.env["sale.order"]
-        # cls.purchase_order_model = cls.env["purchase.order"]
         cls.product_model = cls.env["product.product"]
-        cls.mto_route = cls.env.ref("stock.route_warehouse0_mto")
-        __import__("pdb").set_trace()
 
         cls.product_office_furniture = cls.product_model.create(
             {
@@ -51,8 +26,6 @@ class TestStockSplitPickingKit(SavepointCase):
                 "type": "product",
                 "sale_ok": True,
                 "purchase_ok": True,
-                # "seller_ids": [(0, 0, {"name": cls.seller.id, "price": 200.0})],
-                # "route_ids": [(4, cls.mto_route.id), (4, cls.buy_route.id)],
             }
         )
         cls.tmpl_office_furniture = cls.product_office_furniture.product_tmpl_id
@@ -78,8 +51,6 @@ class TestStockSplitPickingKit(SavepointCase):
                 "type": "product",
                 "sale_ok": False,
                 "purchase_ok": True,
-                # "seller_ids": [(0, 0, {"name": cls.seller.id, "price": 5.0})],
-                # "route_ids": [(4, cls.mto_route.id), (4, cls.buy_route.id)],
             }
         )
         cls.product_garden_furniture = cls.product_model.create(
@@ -88,7 +59,6 @@ class TestStockSplitPickingKit(SavepointCase):
                 "type": "product",
                 "sale_ok": False,
                 "purchase_ok": True,
-                # "seller_ids": [(0, 0, {"name": cls.seller.id, "price": 200.0})],
             }
         )
         cls.tmpl_garden_furniture = cls.product_garden_furniture.product_tmpl_id
@@ -98,9 +68,6 @@ class TestStockSplitPickingKit(SavepointCase):
                 "type": "product",
                 "sale_ok": True,
                 "purchase_ok": True,
-                # "seller_ids": [(0, 0, {"name": cls.seller.id, "price": 30.0})],
-                # "route_ids": [(4, cls.mto_route.id), (4, cls.buy_route.id)],
-                "route_ids": [(4, cls.mto_route.id)],
             }
         )
         cls.product_garden_table = cls.product_model.create(
@@ -109,9 +76,6 @@ class TestStockSplitPickingKit(SavepointCase):
                 "type": "product",
                 "sale_ok": True,
                 "purchase_ok": True,
-                # "seller_ids": [(0, 0, {"name": cls.seller.id, "price": 110.0})],
-                # "route_ids": [(4, cls.mto_route.id), (4, cls.buy_route.id)],
-                "route_ids": [(4, cls.mto_route.id)],
             }
         )
         cls.tmpl_garden_table = cls.product_garden_table.product_tmpl_id
@@ -120,9 +84,6 @@ class TestStockSplitPickingKit(SavepointCase):
                 "name": "GARDEN TABLE TOP",
                 "type": "product",
                 "sale_ok": True,
-                # "seller_ids": [(0, 0, {"name": cls.seller.id, "price": 50.0})],
-                # "route_ids": [(4, cls.mto_route.id), (4, cls.buy_route.id)],
-                "route_ids": [(4, cls.mto_route.id)],
             }
         )
         cls.product_garden_table_leg = cls.product_model.create(
@@ -203,15 +164,6 @@ class TestStockSplitPickingKit(SavepointCase):
             }
         )
 
-    # @classmethod
-    # def create_sale_order(cls, product):
-    #     order_form = Form(cls.sale_order_model)
-    #     order_form.partner_id = cls.customer
-    #     with order_form.order_line.new() as line:
-    #         line.product_id = product
-    #         line.product_uom_qty = 5.0
-    #     return order_form.save()
-
     @classmethod
     def _update_qty_in_location(
         cls, location, product, quantity, package=None, lot=None
@@ -238,15 +190,14 @@ class TestStockSplitPickingKit(SavepointCase):
             "group_id": proc_group,
             "date_planned": today,
             "date_deadline": today,
-            # "warehouse_id": self.wh or False,
-            "warehouse_id": False,
+            "warehouse_id": self.warehouse,
             "company_id": self.env.company,
         }
         procurement = proc_group.Procurement(
             product,
             proc_qty,
             proc_uom,
-            self.src_location,
+            self.dest_location,
             product.name,
             "PROC TEST",
             self.env.company,
@@ -254,96 +205,20 @@ class TestStockSplitPickingKit(SavepointCase):
         )
         proc_group.run([procurement])
 
-    def test_stock_split_picking_kit(self):
+    def test_split_picking_kit_single_split(self):
         self._update_qty_in_location(self.src_location, self.product_garden_table_top, 100)
-        self._create_kit_picking(self.product_garden_furniture, 4)
-        # self.assertTrue(True)
-        # self.picking.action_assign()
-        # wizard = (
-        #     self.env["stock.split.picking"]
-        #     .with_context(active_ids=self.picking.ids)
-        #     .create({"mode": "kit_quantity", "kit_split_quantity": 3})
-        # )
-        # wizard.action_apply()
-
-    # def test_stock_split_picking(self):
-    #     # Picking state is draft
-    #     self.assertEqual(self.picking.state, "draft")
-    #     # We can't split a draft picking
-    #     with self.assertRaises(UserError):
-    #         self.picking.split_process()
-    #     # Confirm picking
-    #     self.picking.action_confirm()
-    #     # We can't split an unassigned picking
-    #     with self.assertRaises(UserError):
-    #         self.picking.split_process()
-    #     # We assign quantities in order to split
-    #     self.picking.action_assign()
-    #     move_line = self.env["stock.move.line"].search(
-    #         [("picking_id", "=", self.picking.id)], limit=1
-    #     )
-    #     move_line.qty_done = 4.0
-    #     # Split picking: 4 and 6
-    #     # import pdb; pdb.set_trace()
-    #     self.picking.split_process()
-
-    #     # We have a picking with 4 units in state assigned
-    #     self.assertAlmostEqual(move_line.qty_done, 4.0)
-    #     self.assertAlmostEqual(move_line.product_qty, 4.0)
-    #     self.assertAlmostEqual(move_line.product_uom_qty, 4.0)
-
-    #     self.assertAlmostEqual(self.move.quantity_done, 4.0)
-    #     self.assertAlmostEqual(self.move.product_qty, 4.0)
-    #     self.assertAlmostEqual(self.move.product_uom_qty, 4.0)
-
-    #     self.assertEqual(self.picking.state, "assigned")
-    #     # An another one with 6 units in state assigned
-    #     new_picking = self.env["stock.picking"].search(
-    #         [("backorder_id", "=", self.picking.id)], limit=1
-    #     )
-    #     move_line = self.env["stock.move.line"].search(
-    #         [("picking_id", "=", new_picking.id)], limit=1
-    #     )
-
-    #     self.assertAlmostEqual(move_line.qty_done, 0.0)
-    #     self.assertAlmostEqual(move_line.product_qty, 6.0)
-    #     self.assertAlmostEqual(move_line.product_uom_qty, 6.0)
-
-    #     self.assertAlmostEqual(new_picking.move_lines.quantity_done, 0.0)
-    #     self.assertAlmostEqual(new_picking.move_lines.product_qty, 6.0)
-    #     self.assertAlmostEqual(new_picking.move_lines.product_uom_qty, 6.0)
-
-    #     self.assertEqual(new_picking.state, "assigned")
-
-    # def test_stock_split_picking_wizard_move(self):
-    #     self.move2 = self.move.copy()
-    #     self.assertEqual(self.move2.picking_id, self.picking)
-    #     wizard = (
-    #         self.env["stock.split.picking"]
-    #         .with_context(active_ids=self.picking.ids)
-    #         .create({"mode": "move"})
-    #     )
-    #     wizard.action_apply()
-    #     self.assertNotEqual(self.move2.picking_id, self.picking)
-    #     self.assertEqual(self.move.picking_id, self.picking)
-
-    # def test_stock_split_picking_wizard_selection(self):
-    #     self.move2 = self.move.copy()
-    #     self.assertEqual(self.move2.picking_id, self.picking)
-    #     wizard = (
-    #         self.env["stock.split.picking"]
-    #         .with_context(active_ids=self.picking.ids)
-    #         .create({"mode": "selection", "move_ids": [(6, False, self.move2.ids)]})
-    #     )
-    #     wizard.action_apply()
-    #     self.assertNotEqual(self.move2.picking_id, self.picking)
-    #     self.assertEqual(self.move.picking_id, self.picking)
-
-    # def test_stock_picking_split_off_moves(self):
-    #     with self.assertRaises(UserError):
-    #         # fails because we can't split off all lines
-    #         self.picking._split_off_moves(self.picking.move_lines)
-    #     with self.assertRaises(UserError):
-    #         # fails because we can't split cancelled pickings
-    #         self.picking.action_cancel()
-    #         self.picking._split_off_moves(self.picking.move_lines)
+        pickings_before = self.env["stock.picking"].search([])
+        self._create_kit_picking(self.product_garden_table, 4)
+        pickings_after = self.env["stock.picking"].search([])
+        picking = pickings_after - pickings_before
+        self.assertTrue(picking)
+        picking.action_assign()
+        self.assertEqual(picking.state, "assigned")
+        wizard = (
+            self.env["stock.split.picking"]
+            .with_context(active_ids=picking.ids)
+            .create({"mode": "kit_quantity", "kit_split_quantity": 3})
+        )
+        wizard.action_apply()
+        new_picking = self.env["stock.picking"].search([]) - pickings_after
+        self.assertTrue(new_picking)
--- stock_split_picking_kit/wizards/stock_split_picking.py
+++ stock_split_picking_kit/wizards/stock_split_picking.py
@@ -2,6 +2,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
+from odoo.tools import groupby
+from odoo.exceptions import UserError
 
 
 class StockSplitPicking(models.TransientModel):
@@ -13,19 +15,57 @@ class StockSplitPicking(models.TransientModel):
     )
     kit_split_quantity = fields.Integer()
 
-
     def _apply_kit_quantity(self):
         # TODO: split by the kit quantity set in the ui
         # Questions ?
         # - What if there is move for products that are not kits ?
         # - Should we have a default or fail if the split quanity is zero ?
+
         pickings = self.env["stock.picking"]
         for picking in self.mapped("picking_ids"):
             pickings |= self._split_by_kit_quantity(picking)
         return pickings
 
     def _split_by_kit_quantity(self, picking):
-        return picking
+        all_pickings = picking
+        current_picking = picking
+        new_picking = self.env["stock.picking"]
+        used_slots = 0
+        for bom, bom_move_list in groupby(current_picking.move_lines, key=lambda move: move.bom_line_id.bom_id):
+            if bom.type != "phantom":
+                raise UserError("Should it not be allowed if not a full kit picking ?")
+            moves = self.env["stock.move"].browse([move.id for move in bom_move_list])
+            order_qty = max(moves.mapped("product_qty")) # Just give the maximum possible
+            filters = {
+                "incoming_moves": lambda m: m.location_id.usage == "supplier"
+                and (
+                    not m.origin_returned_move_id
+                    # or (m.origin_returned_move_id and m.to_refund)
+                ),
+                "outgoing_moves": lambda m: m.location_id.usage != "supplier"
+                # and m.to_refund,
+            }
+            kit_quantity = moves._compute_kit_quantities(
+                    bom.product_id, order_qty, bom , filters
+            )
+            kit_quantity = abs(kit_quantity)
+            if used_slots + kit_quantity > self.kit_split_quantity:
+                # Split the picking for a smaller number of kits
+                # It means splitting some moves quantity
+                kit_to_split = kit_quantity - self.kit_split_quantity - used_slots
+                new_moves = self.env["stock.move"]
+                for move in moves:
+                    new_move_vals = move._split(move.bom_line_id.product_qty * kit_to_split)
+                    if new_move_vals:
+                        new_moves |= self.env["stock.move"].create(new_move_vals)
+                new_picking = picking._create_split_backorder()
+                new_moves.write({"picking_id": new_picking.id})
+                new_moves.mapped("move_line_ids").write({"picking_id": new_picking.id})
+                all_pickings |= new_picking
+                __import__("pdb").set_trace()
+            else:
+                used_slots = used_slots + kit_quantity
+        return all_pickings
 
 
     # def _apply_done(self):
